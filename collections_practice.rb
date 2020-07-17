a37c8957ef161dda49b9edc94e0dd920690d327a
Jill Short <jillian.b.short@gmail.com>
2020-07-17 00:41:07 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,3 +1,4 @@
+#require 'pry'
 def sort_array_asc(integers)
   integers.sort do |a , b|
     a <=> b 
@@ -20,4 +21,33 @@ def swap_elements(swaps)
   swaps[0], swaps[1], swaps[2] = swaps[0], swaps[2], swaps[1]
 end
 
-  
+def reverse_array(integers)
+  integers.reverse
+end
+
+def kesha_maker(names)
+  names.each do |name|
+    print name.split("")
+    name[2] = "$"
+  end
+end
+
+def find_a(words)
+  words.select {|word| word.start_with?("a")}
+end
+
+def sum_array(numbers)
+  numbers.inject {|sum, n| sum + n}
+end
+
+def add_s(singular)
+  singular.each_with_index.collect do |plural, index|
+    if index != 1
+      plural + "s"
+    else
+      plural
+    end
+  end
+end
+
+#{ |x,i| i == 1 || x[-1] == 's' ? x : x + 's' }

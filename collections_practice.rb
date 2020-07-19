d69973345db841463c02d2fb81a5d61a20fad11d
Billy Gunn, Jr <bgunnjr@gmail.com>
2020-07-19 00:03:47 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,36 @@
+def sort_array_asc(array)
+  array.sort do |a, b|
+  a <=> b
+  end
+end
 
+def sort_array_desc(array)
+  array.sort! do |a, b|
+  a <=> b
+  end
+end
+
+def sort_array_char_count(array)
+  array
+  end
+end
+
+def swap_elements(array)
+  
+  end
+end
+
+def reverse_array(array)
+  
+  end
+end
+
+def kesha_maker(array)
+  
+  end
+end
+
+def find_a(array)
+  
+  end
+end
